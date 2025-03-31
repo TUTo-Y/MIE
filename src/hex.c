1b73a394d4cba481c7c99b8d6d4b77be3e5df00e
TUTo <2219878724@qq.com>
2025-03-31 14:40:52 +0800

    第一次改动前的版本

--- src/hex.c
+++ src/hex.c
@@ -3,11 +3,11 @@
 static const uint8_t hex_lookup[256] = {
     [0] = '0', [1] = '1', [2] = '2', [3] = '3', [4] = '4', [5] = '5', [6] = '6', [7] = '7', [8] = '8', [9] = '9', [10] = 'a', [11] = 'b', [12] = 'c', [13] = 'd', [14] = 'e', [15] = 'f', ['0'] = 0, ['1'] = 1, ['2'] = 2, ['3'] = 3, ['4'] = 4, ['5'] = 5, ['6'] = 6, ['7'] = 7, ['8'] = 8, ['9'] = 9, ['a'] = 10, ['b'] = 11, ['c'] = 12, ['d'] = 13, ['e'] = 14, ['f'] = 15, ['A'] = 10, ['B'] = 11, ['C'] = 12, ['D'] = 13, ['E'] = 14, ['F'] = 15};
 
-static uint8_t hex_to_bin(char c)
+static inline uint8_t hex_to_bin(char c)
 {
     return hex_lookup[(unsigned char)c];
 }
-static char bin_to_hex(uint8_t b)
+static inline char bin_to_hex(uint8_t b)
 {
     return hex_lookup[b];
 }
